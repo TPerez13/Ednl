a53fb2ef2af158a795e0bcdf17242f13d087bc43
TPerez13 <pabloperezdiaz22@gmail.com>
2022-03-16 11:21:17 +0100

    Ej cifrado

---
+++ Arboles/Practica_2/EjCifrado.cpp
@@ -0,0 +1,3 @@
+/*-Dado una arbol, con el mensaje  "Que me ha dado antes", eeeeh, debnes sacar el mensaje ecrito en el arbol. 
+2- Recorrer el mismo arbol en inorden, y sacar el mensaje de recorrerlo en inorden**/
+
