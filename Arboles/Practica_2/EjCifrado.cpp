56606d193e5c1108183e943cd23fe5c8efa7b3e0
TPerez13 <pabloperezdiaz22@gmail.com>
2022-03-23 12:18:02 +0100

    Ejercicio de cifrado

--- Arboles/Practica_2/EjCifrado.cpp
+++ Arboles/Practica_2/EjCifrado.cpp
@@ -8,7 +8,7 @@ void descifrar_mensaje_Rec(Abin<tElto>::nodo n,Abin &A,)
         
     }
 }
-
+/*
 void construir_Arbol(char* cad_cif,Abin<tElto>::nodo n, Abin<tElto> &A,aux)
 {
     if(A.raiz() == Abin<tElto>::NODO_NULO)
@@ -21,7 +21,7 @@ void construir_Arbol(char* cad_cif,Abin<tElto>::nodo n, Abin<tElto> &A,aux)
     {
         if(cad_cif[aux] != '/0')
         {
-            if(A.profundidad(n) != (log2(cad_cif)))
+            if(A.profundidad(n) < (log2(cad_cif)))
             {
                 A.insertarHijoIzqdo(n,cad_cif[aux]);
                 aux++;
@@ -42,7 +42,7 @@ void construir_Arbol(char* cad_cif,Abin<tElto>::nodo n, Abin<tElto> &A,aux)
         }
     }
 }
-
+*/
 
 char* descifrar_mensaje(char* cad_cif)
 {
@@ -53,4 +53,32 @@ char* descifrar_mensaje(char* cad_cif)
     Abin<tElto> A;
 
     construir_Arbol(cad_cif,A);
+}
+
+
+void construir_Arbol(char* cad_cif,int h,int i, int j,Abin<tElto>::nodo n, Abin<tElto> &A)
+{
+    A.insertarHijoIzqdo(n,cad_cif[i+1]);
+    
+    bool bandera;
+    if(j-i-pow(2,h) > pow(2,h-1)-1)
+    {
+        A.insertarHijoDrcho(n,cad_cif[i+pow(2,h)]);
+        bandera = true;
+    }
+    else
+    {
+        A.insertarHijoDrcho(n,cad_cif[j-pow(2,h-1)+1]);
+        bandera = false;
+    }
+
+    construir_Arbol(cad_cif,h,i+1,(j/2)+1,A.hijoIzqdo(n),A);
+    
+    if(bandera)
+    {
+        construir_Arbol(cad_cif,h,i+pow(2,h),j,A.hijoDrcho(n),A)
+    }else
+    {
+        construir_Arbol(cad_cif,h,j-pow(2,h-1)+1,j,A.hijoDrcho(n),A)
+    }
 }
