32881d4481448d1f2fc6607e3122d4067e5cc08b
TPerez13 <pabloperezdiaz22@gmail.com>
2022-03-24 17:36:27 +0100

    Refinando ejs

---
+++ Arboles/Examen1/Desiquilibrio_prac3.cpp
@@ -0,0 +1,33 @@
+int calculardesequilibrio_Rec(Abin <tElto>::nodo n, Abin<tElto> &A)
+{
+    if(n != NODO_NULO)
+    {
+       return 0
+    }
+    else
+    {
+        if()
+    }
+}
+
+
+if(n!=NODO_NULO)
+{
+    do
+    {
+        if(!C.vacia())
+        {
+            n= c.primera();
+            c.pop();
+        }
+        
+        //Procesar a;
+
+        if(A.hijoIzqdo(n)!=NODO_NULO)
+            c.push(A.hijoIzqdo(n));
+        if(A.hijoDrcho(n)!=NODO_NULO);
+            c.push(A.hijoDrcho(n));
+        /* code */
+    } while (!C.vacia());
+    
+}
