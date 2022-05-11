8f66fe466ff5ae4fd64a55da9e71562c29b12c0d
TPerez13 <pabloperezdiaz22@gmail.com>
2022-05-11 12:17:50 +0200

    Practica 7 Dlh

---
+++ Grafos/Practica_7/Ejercicio2.cpp
@@ -0,0 +1,6 @@
+//Si no se dice expresamente el cmaino como secuencia de nodos asumimos que aceptamos las matrices p como camino
+
+//No aparece la palbra grafo, matriz de costes -> La funcion no tiene como parametro de entrada un grafo.
+
+
+//Te dan un laberinto y lo tienes que representar mediante un grafo
